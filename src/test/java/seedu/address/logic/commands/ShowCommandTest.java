eded98373d02c3f373ebad8535d83e469f9b9150
codebreaker64 <64005011+codebreaker64@users.noreply.github.com>
2025-03-12 12:55:14 +0800

    Add show command

---
+++ src/test/java/seedu/address/logic/commands/ShowCommandTest.java
@@ -0,0 +1,94 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showCustomerAtIndex;
+import static seedu.address.testutil.TypicalCustomers.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CUSTOMER;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_CUSTOMER;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.customer.Customer;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for
+ * {@code ShowCommand}.
+ */
+public class ShowCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_validIndexUnfilteredList_success() {
+        Customer customerToShow = model.getFilteredCustomerList().get(INDEX_FIRST_CUSTOMER.getZeroBased());
+        ShowCommand showCommand = new ShowCommand(INDEX_FIRST_CUSTOMER);
+
+        String expectedMessage = String.format(ShowCommand.MESSAGE_SHOW_CUSTOMER_SUCCESS,
+                Messages.format(customerToShow));
+
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+
+        assertCommandSuccess(showCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidIndexUnfilteredList_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredCustomerList().size() + 1);
+        ShowCommand showCommand = new ShowCommand(outOfBoundIndex);
+
+        assertCommandFailure(showCommand, model, Messages.MESSAGE_INVALID_CUSTOMER_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_invalidIndexFilteredList_throwsCommandException() {
+        showCustomerAtIndex(model, INDEX_FIRST_CUSTOMER);
+
+        Index outOfBoundIndex = INDEX_SECOND_CUSTOMER;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getCustomerList().size());
+
+        ShowCommand showCommand = new ShowCommand(outOfBoundIndex);
+
+        assertCommandFailure(showCommand, model, Messages.MESSAGE_INVALID_CUSTOMER_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        ShowCommand showFirstCommand = new ShowCommand(INDEX_FIRST_CUSTOMER);
+        ShowCommand showSecondCommand = new ShowCommand(INDEX_SECOND_CUSTOMER);
+
+        // same object -> returns true
+        assertTrue(showFirstCommand.equals(showFirstCommand));
+
+        // same values -> returns true
+        ShowCommand showFirstCommandCopy = new ShowCommand(INDEX_FIRST_CUSTOMER);
+        assertTrue(showFirstCommand.equals(showFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(showFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(showFirstCommand.equals(null));
+
+        // different customer -> returns false
+        assertFalse(showFirstCommand.equals(showSecondCommand));
+    }
+
+    @Test
+    public void toStringMethod() {
+        Index targetIndex = Index.fromOneBased(1);
+        ShowCommand showCommand = new ShowCommand(targetIndex);
+        String expected = ShowCommand.class.getCanonicalName() + "{targetIndex=" + targetIndex + "}";
+        assertEquals(expected, showCommand.toString());
+    }
+
+}
