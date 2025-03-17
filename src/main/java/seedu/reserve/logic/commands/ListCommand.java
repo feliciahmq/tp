02ec06fbfb3e73b8e4b7e3a57fe9ff7d082912e9
JohannsenLum <lumjohannsen@gmil.com>
2025-03-18 00:42:02 +0800

    Update ListCommand to include error message

--- src/main/java/seedu/reserve/logic/commands/ListCommand.java
+++ src/main/java/seedu/reserve/logic/commands/ListCommand.java
@@ -3,7 +3,12 @@ package seedu.reserve.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.reserve.model.Model.PREDICATE_SHOW_ALL_RESERVATIONS;
 
+import java.util.List;
+
+import seedu.reserve.logic.Messages;
+import seedu.reserve.logic.commands.exceptions.CommandException;
 import seedu.reserve.model.Model;
+import seedu.reserve.model.reservation.Reservation;
 
 /**
  * Lists all reservations in the reservation book to the user.
@@ -15,8 +20,15 @@ public class ListCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Listed all reservations";
 
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+
+        List<Reservation> lastShownList = model.getFilteredReservationList();
+
+        if (lastShownList.isEmpty()) {
+            throw new CommandException(Messages.MESSAGE_EMPTY_RESERVATION_LIST);
+        }
+
         model.updateFilteredReservationList(PREDICATE_SHOW_ALL_RESERVATIONS);
         return new CommandResult(MESSAGE_SUCCESS);
     }
