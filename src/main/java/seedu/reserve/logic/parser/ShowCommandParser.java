19ec6b6d8c5c7c268eb76cf992b6d28c1ebf804d
codebreaker64 <64005011+codebreaker64@users.noreply.github.com>
2025-04-03 19:06:06 +0800

    Fix checkstyle

--- src/main/java/seedu/reserve/logic/parser/ShowCommandParser.java
+++ src/main/java/seedu/reserve/logic/parser/ShowCommandParser.java
@@ -18,7 +18,7 @@ public class ShowCommandParser implements Parser<ShowCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public ShowCommand parse(String args) throws ParseException {
-        if (args.trim().isEmpty()) {  // Better empty check that handles whitespace
+        if (args.trim().isEmpty()) { // Better empty check that handles whitespace
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, ShowCommand.MESSAGE_USAGE));
         }
